3a1c235cd17a7f6b346ecc166ec358f88dde210c
Carlos Andrés Martinez <cmartinez@quamto.com>
2017-03-17 16:32:40 -0500

    Conditionals

---
+++ Conditional/Conditional/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conditional if example number prime
+            int numberToInput = 0;
+            numberToInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number is {0}",numberToInput);
+            Console.ReadKey();
+
+        }
+    }
+}
