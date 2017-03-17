afa8d835a722b715dc28b2de90de46d1ae6811e5
Carlos Andrés Martinez <cmartinez@quamto.com>
2017-03-17 15:39:26 -0500

    Type of data

--- example1/example1/Program.cs
+++ example1/example1/Program.cs
@@ -12,6 +12,20 @@ namespace example1
         {
             // First program, hello world
             Console.WriteLine("Hello world");
+
+            // Type of data, everthing is the object
+            int age = 24;
+            decimal d = 1.0m;
+            float height = 1.69f;
+            double nD = 1.0d;
+            string name = "carlos";
+            bool flag = true;
+            DateTime date = DateTime.Now;
+            Console.WriteLine("I have {0} years old", age);
+            Console.WriteLine("My height is {0:f}cm", height);
+            Console.WriteLine("My name is {0}",name);
+            Console.WriteLine("The time is {0}", date);
+
             Console.ReadKey();
         }
     }
