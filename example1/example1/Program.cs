0979be04a0f1c2600c42c58312642ea9b721687c
Carlos Andrés Martinez <cmartinez@quamto.com>
2017-03-17 15:03:19 -0500

    hello world

--- example1/example1/Program.cs
+++ example1/example1/Program.cs
@@ -10,6 +10,9 @@ namespace example1
     {
         static void Main(string[] args)
         {
+            // First program, hello world
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
         }
     }
 }
