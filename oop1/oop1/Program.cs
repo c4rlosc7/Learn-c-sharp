4f6b800bdf7f6ccdbe126b970f010f2b7bc7ab1c
Carlos Andrés Martinez <cmartinez@quamto.com>
2017-03-21 08:00:07 -0500

    init with oop clsPerson

---
+++ oop1/oop1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsPerson student = new clsPerson();
+            student.Name = "Carlos";
+            student.LastName = "Martinez";
+            student.Age = 26;
+            Console.WriteLine(student.info());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ oop1/oop1/clsPerson.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop1
+{
+    public class clsPerson
+    {
+        public string Name;
+
+        public string LastName;
+
+        public int Age;
+
+        // CONSTRUCTOR 
+        public clsPerson()
+        {
+            Name = "";
+            LastName = "";
+            Age = 0;
+        }
+
+        public string info()
+        {
+            return "Name: " + Name + " LastName: " + LastName + " Age: " + Age;
+        }
+    }
+}
